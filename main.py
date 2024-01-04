1b4d1ba83187aedca13c2814e3e56e4f4cd6d150
郭文睿 <87920151@qq.com>
2024-01-04 22:35:33 +0800

    交互式文档配置信息

--- main.py
+++ main.py
@@ -8,7 +8,24 @@ from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-app = FastAPI(title="学习FastAPI框架文档", description="以下是关于框架文档的介绍和描述", version="0.0.1")
+app = FastAPI(title="学习FastAPI框架文档", description="以下是关于框架文档的介绍和描述", version="0.0.1",
+              # debug=True,
+              terms_of_service="https://terms.团队的官网网站/",
+              deprecated=False,  # 标记API废弃
+              contact={"name": "邮件接受者信息", "url": "https://xxx.cc", "email": "87920151@qq.com"},
+              license_info={"name": "版权信息说明License v3.0", "url": "https://www.baidu.com"},
+              openapi_tags=[
+                  {"name": "接口分组", "description": "接口分组信息说明"}
+              ],
+              servers=[
+                  {"url": "/", "description": "本地调试环境"},
+                  {"url": "https://xx.xx.com", "description": "线上测试环境"},
+                  {"url": "https://xx2.xx2.com", "description": "线上生产环境"}
+              ],
+              # docs_url=None,  # 关闭docs访问
+              # redoc_url=None, # 关闭redoc访问
+              # openapi_url=None, #文档全部关闭访问
+              )
 
 templates = Jinja2Templates(directory=f"{pathlib.Path.cwd()}/templates/")
 staticfiles = StaticFiles(directory=f"{pathlib.Path.cwd()}/static/")
@@ -23,6 +40,7 @@ def app_hello():
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    # 1 / 0
     return templates.TemplateResponse("index.html", {"request": request})
 
 
